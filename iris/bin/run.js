53d2cab81661bdc513130d5196a08445fbaae26c
Sylum Mastropaolo <sylum.mastropaolo@gmail.com>
2018-09-18 17:58:09 -0400

    Added eslint

--- iris/bin/run.js
+++ iris/bin/run.js
@@ -14,12 +14,12 @@ const witClient = require('../server/witClient')(witToken);
 const slackToken = config.slackToken;
 const slackLogLevel = 'verbose';
 
-const serviceRegistry = service.get('serviceRegistry')
+const serviceRegistry = service.get('serviceRegistry');
 const rtm = slackClient.init(slackToken, slackLogLevel, witClient, serviceRegistry);
 rtm.start();
 
 // This only starts the server if the connection is succesful to slack
-slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000))
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
 
 server.on('listening', function() {
     console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
